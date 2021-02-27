3ed64a5af1495c4341e83c6d70c1c21a906e674c
AmirTheWiseWizzard <you@example.com>
2021-02-27 14:02:51 +0330

    Login function Created

--- SHADAttendance/SHADAttendance.py
+++ SHADAttendance/SHADAttendance.py
@@ -1,10 +1,29 @@
 
 from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
 
-driver = Chrome()
+def main():
+    driver = Chrome()
+    driver.get('https://web.shad.ir/')
+    login()
 
-driver.get('https://web.shad.ir/')
-cookies = driver.get_cookies()
-for cookie in cookies:
-    print(cookie)
+    
 
+
+def login():
+    num = input_phonenumber()
+    driver.find_element_by_name("phone_number").send_keys(num)
+    driver.find_element_by_class_name("login_head_submit_btn").click() 
+    driver.find_element_by_class_name("btn.btn-md.btn-md-primary").click() 
+    rCode = input("Enter The Code : ")
+    driver.find_element_by_name("phone_code").send_keys(rCode)
+    return
+
+def input_phonenumber():
+    numfile = open('MobileNumber.txt', 'r')
+    number = numfile.readline()
+    return number
+
+
+if __name__ == "__main__":
+    main()
