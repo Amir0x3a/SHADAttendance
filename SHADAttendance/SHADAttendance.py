98b53550aa28147b6d7d97374e9bdc5eea4f856d
AmirTheWiseWizzard <you@example.com>
2021-03-16 16:25:26 +0330

    cookie is Ok-- but we need save LOCAl_STORAGE instead

--- SHADAttendance/SHADAttendance.py
+++ SHADAttendance/SHADAttendance.py
@@ -2,6 +2,8 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.keys import Keys
 import ctypes
+import os
+import pickle
 import bs4
 
 
@@ -9,17 +11,19 @@ class main:
 
 
     def start(self):
-        self.login()
+        driver = Chrome()
+        driver.get('https://web.shad.ir/')
+        self.login(driver)
         # Wait for user to select chat
-        while not self.driver.find_elements_by_class_name("im_dialog_wrap.active"):
+        while not driver.find_elements_by_class_name("im_dialog_wrap.active"):
             ctypes.windll.user32.MessageBoxW(0, "Choose your chat", "Wait For User ...", 0)
-        self.driver.find_element_by_class_name("im_dialog_wrap.active").click()
+        driver.find_element_by_class_name("im_dialog_wrap.active").click()
         # Scroll Conversation pannel
         # Do while see difference in date text and keep last date
         # scrl_element = self.driver.find_element_by_class_name("im_history_col")
          
         while True:
-            split_list = self.driver.find_elements_by_class_name("im_message_date_split_text")
+            split_list = driver.find_elements_by_class_name("im_message_date_split_text")
             
             split_text = [x.text for x in split_list] 
             if not split_text.count(split_text[0]) == len(split_text) and len(split_text)>0:
@@ -27,7 +31,7 @@ class main:
                 split_text = list(set(split_text) - set(last_list))[0]
                 break
             last_list = split_text
-            self.driver.execute_script("arguments[0].scrollIntoView()", split_list[-1])
+            driver.execute_script("arguments[0].scrollIntoView()", split_list[-1])
 
             
         self.extract_datahtml(split_text)
@@ -41,18 +45,32 @@ class main:
         print(author_tg)
 
 
-    def __init__(self):
-        self.driver = Chrome()
-        self.driver.get('https://web.shad.ir/')
+    def login(self, driver):
+        self.load_cookie(driver)
+        # Agar Phone number nadarim SAVE new Cookie
+        if len(driver.find_elements_by_name("phone_number"))<1:
+            return
+        else:
+            num = self.input_phonenumber()
+            driver.find_element_by_name("phone_number").send_keys(num)
+            driver.find_element_by_class_name("login_head_submit_btn").click() 
+            driver.find_element_by_class_name("btn.btn-md.btn-md-primary").click() 
+            rCode = input("Enter The Code : ")
+            driver.find_element_by_name("phone_code").send_keys(rCode)
+            self.save_cookie(driver)    
+            return
 
-    def login(self):
-        num = self.input_phonenumber()
-        self.driver.find_element_by_name("phone_number").send_keys(num)
-        self.driver.find_element_by_class_name("login_head_submit_btn").click() 
-        self.driver.find_element_by_class_name("btn.btn-md.btn-md-primary").click() 
-        rCode = input("Enter The Code : ")
-        self.driver.find_element_by_name("phone_code").send_keys(rCode)
-        return
+    def save_cookie(self, driver):
+        with open('cookie.dat', 'wb') as filehandler:
+            pickle.dump(driver.get_cookies(), filehandler)
+
+    def load_cookie(self, driver):  
+        cookiePath = 'cookie.dat'
+        if os.path.getsize(cookiePath) > 0 and os.path.exists(cookiePath):
+            with open(cookiePath, 'rb') as cookiesfile:
+                cookies = pickle.load(cookiesfile)
+                for cookie in cookies:
+                    driver.add_cookie(cookie)
     
     def input_phonenumber(self):
         numfile = open('MobileNumber.txt', 'r')
